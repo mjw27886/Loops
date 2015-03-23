1f53b602a6b242e16ec9b1d9ff85f40da2222b0a
mjw27886 <mjw27886@email.vccs.edu>
2015-03-23 15:39:39 -0400

    Update loops2.java

--- loops2.java
+++ loops2.java
@@ -1,31 +1,29 @@
 import javax.swing.JOptionPane;
 
 public class loops2 {
-	public static void main(String [] args) {
-			String user, pass, user1, pass1;
-					final int LIMIT = 2;
-							int count = 0;
+public static void main(String [] args) {
+String user, pass, user1, pass1;
+final int LIMIT = 2;
+int count = 0;
 									
 											
-													user = JOptionPane.showInputDialog("Enter your Username");
-															pass = JOptionPane.showInputDialog("Enter your password");
-																	user1 = JOptionPane.showInputDialog("Enter your Username");
-																			pass1 = JOptionPane.showInputDialog("Enter your password");
+user = JOptionPane.showInputDialog("Enter your Username");
+pass = JOptionPane.showInputDialog("Enter your password");
+user1 = JOptionPane.showInputDialog("Enter your Username");
+pass1 = JOptionPane.showInputDialog("Enter your password");
 																					
-																							while (count < LIMIT) {
-																										
-																												if (user.equals(user1) && (pass.equals(pass1))) {
-																															JOptionPane.showMessageDialog(null, "Welcome " + user);
-																																	System.exit(0); }
-																																			
-																																					else {
-																																							JOptionPane.showMessageDialog(null, "Incorrect!! please try again");
-																																									user1 = JOptionPane.showInputDialog("Enter your Username");
-																																											pass1 = JOptionPane.showInputDialog("Enter your password");
-																																													count++;}}
-																																															JOptionPane.showMessageDialog(null, "Please contact your adminstrator to unlock your account!");
-																																																	System.exit(0);
+while (count < LIMIT) {
 
-																																																	}
-																																																	}
-
+if (user.equals(user1) && (pass.equals(pass1))) {
+JOptionPane.showMessageDialog(null, "Welcome " + user);
+System.exit(0); }
+	
+else {
+JOptionPane.showMessageDialog(null, "Incorrect!! please try again");
+user1 = JOptionPane.showInputDialog("Enter your Username");
+pass1 = JOptionPane.showInputDialog("Enter your password");
+count++;}}
+JOptionPane.showMessageDialog(null, "Please contact your adminstrator to unlock your account!");
+System.exit(0);
+}
+}
